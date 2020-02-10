b3007f0ee3f85a5764efe05ace998cd1b16ba0a1
Derick Del Rosario <derickmitchel@icloud.com>
2020-02-10 11:39:44 -0800

    welcome function

---
+++ terminalCooking.js
@@ -0,0 +1,10 @@
+/* This project is inspired by Cooking Adventures, which is a catering game wherein you serve customers customized orders 
+   with the goal of achieving x gross profit under a time limit. There are many scenarios with different types of cuisine
+   and many different ways to prepare food, and your time management / reflexes are very often tested. You are also able 
+   to upgrade your kitchen, cookware, product, and even your kitchen. There are also unlockable outfits and the like that
+   I will not be implementing in this terminal edition. */
+
+function boot() {
+    console.log('Welcome to Terminal Cook!');
+    console.log('[ N ] New Game | [ L ] Load Saved Game | [ H ] Help');
+};
